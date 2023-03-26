1835a659222b08f2ca6ad82f42e6133e16177fb7
cafesito <leliasgomer@gmail.com>
2023-03-26 17:01:14 -0500

    personalizo el js con mi nombre

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log('hi')
-//Configurando cosas nuevas en estes archivo
+//haciendo un event al cargar la página
+window.addEventListener('load', ()=>{
+    console.log('este es Elias')
+    const h1 = document.createElement('h1')
+    h1.innerHTML = 'Este es Elias'
+    document.documentElement.appendChild(h1)
+})
