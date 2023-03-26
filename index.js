72ee1d032cac2a4b946d5cb8e6b7735c743312ff
cafesito <leliasgomer@gmail.com>
2023-03-26 17:04:54 -0500

    se configuro nombre

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-console.log('hi')
-//Configurando cosas nuevas en estes archivo
+window.addEventListener('load', ()=>{
+    const h1 = document.createElement('h1')
+    h1.innerHTML = 'Esta es la rama de Maria'
+    document.documentElement.appendChild(h1)
+})
